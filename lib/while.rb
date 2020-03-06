72e82061bf95bb1d6840ecbd615c379841494c99
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-06 20:38:55 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,13 +5,9 @@
 
 def using_while 
   levitation_force = 6
-  until leviation force == 10
+  until leviation force < 10
     puts "Wingardium Leviosa"
   	levitation_force += 1
   end
 
 end
-
-def using_until
-  
-end 
