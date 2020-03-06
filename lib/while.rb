7d6f4d66cf3cb543bd1382872e82ee4331eddc7f
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-06 20:56:00 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,11 @@
 
 # puts Wingardium Leviosa
-# using_while levitation force is incremented to 10
+# using_while levitation force is less than 10
 # counter = 1
 
 def using_while 
   levitation_force = 6
-  while levitation force = 10
+  while levitation force < 10
     puts "Wingardium Leviosa"
   	levitation_force += 1
   end
